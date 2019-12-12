c1f824a5212ce97ba36e812e721e3ba1b41d78db
Rafael Faita <rafael.faita.ext@nokia.com>
2019-12-12 14:36:26 +0000

    iot-gtw

--- src/main/java/com/iot/tsa/TimeSeriesAggregationApplication.java
+++ src/main/java/com/iot/tsa/TimeSeriesAggregationApplication.java
@@ -2,6 +2,10 @@ package com.iot.tsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 public class TimeSeriesAggregationApplication {
@@ -10,5 +14,9 @@ public class TimeSeriesAggregationApplication {
         SpringApplication.run(TimeSeriesAggregationApplication.class, args);
     }
 
+    @Bean
+    public Filter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
 
 }
--- src/main/java/com/iot/tsa/controller/SensorDataController.java
+++ src/main/java/com/iot/tsa/controller/SensorDataController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("timeseries")
+@RequestMapping("/timeseries")
 public class SensorDataController {
 
 
